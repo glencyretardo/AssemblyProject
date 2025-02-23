7db5e016a2e980ca780884a3e06f931a4289e8eb
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2025-02-23 15:28:07 +0800

    same version

--- try.asm
+++ try.asm
@@ -24,20 +24,94 @@ continuePrompt db 10, 9, "Do you want to continue? [y/n]: ", 0
 choice db 4 dup(0)
 continueChoice db 4 dup(0)
 
-subjects1_1 db 10, 9, "Subjects for 1st Year: 1st Sem", 10, 9, "    Understanding the Self", 10, 9, "    Mathematics in the Modern World", 10, 9, "   Purposive Coomunication", 10, 9, "   Rizal's Life and Work", 10, 9, "    Foundation of Special Needs", 10, 9, "    Movement Competency Training", 10, 9,"    National Service Training Program 1", 10, 9, 0
-subjects1_2 db 10, 9, "Subjects for 1st Year: 2nd Sem:", 10, 9, "    Understanding the Self", 10, 9, "    Mathematics in the Modern World", 10, 9, "    National Service Training Program 1", 10, 9, 0
+subjects1_1 db 10, 9, "Subjects for 1st Year: 1st Sem", 10, 9
+            db "    Understanding the Self", 10, 9
+            db "    Mathematics in the Modern World", 10, 9
+            db "    Purposive Communication w/ Interactive Learning", 10, 9
+            db "    The Child and Adolescent Learners and Learning Principles", 10, 9
+            db "    Rizal's Life and Work", 10, 9
+            db "    Foundation of Special and Inclusive Education", 10, 9
+            db "    Movement Competency Training", 10, 9
+            db "    National Service Training Program 1", 10, 9, 0
 
-subjects2_1 db 10, 9, "Subjects for 2nd Year: 1st Sem", 10, 9, "    Assessment of Learning 1", 10, 9, "    Teaching Math in the Primary Grades", 10, 9, "    Development of Individualized Education Plans", 10, 9, 0
-subjects2_2 db 10, 9, "Subjects for 2nd Year: 2nd Sem", 10, 9, "    Assessment of Learning 1", 10, 9, "    Teaching Math in the Primary Grades", 10, 9, "    Development of Individualized Education Plans", 10, 9, 0
+subjects1_2 db 10, 9, "Subjects for 1st Year: 2nd Sem", 10, 9
+            db "    Strategic Lesson Planning and Design", 10, 9
+            db "    Reading Comprehension", 10, 9
+            db "    The Contemporary World", 10, 9
+            db "    Technology for Teaching and Learning 1", 10, 9
+            db "    Facilitating Learner-Centered Teaching", 10, 9
+            db "    Science, Technology, and Society", 10, 9
+            db "    Exercise-based Fitness Activities", 10, 9
+            db "    National Service Training Program 2", 10, 9, 0
 
-subjects3_1 db 10, 9, "Subjects for 3rd Year: 1st Sem", 10, 9, "    The Teaching Profession", 10, 9, "    Teaching PE and Health in Elementary Grades", 10, 9, "    Early Childhood Inclusive Education", 10, 9, 0
-subjects3_2 db 10, 9, "Subjects for 3rd Year: 2nd Sem", 10, 9, "    The Teaching Profession", 10, 9, "    Teaching PE and Health in Elementary Grades", 10, 9, "    Early Childhood Inclusive Education", 10, 9, 0
+subjects1_summer db 10, 9, "Subjects for 1st Year: Summer", 10, 9
+            db "    Educational Assessment of Students with Additional Needs", 10, 9
+            db "    Art Appreciation", 10, 9
+            db "    Content and Pedagogy in the Mother-tongue", 10, 9
+            
+subjects2_1 db 10, 9, "Subjects for 2nd Year: 1st Sem", 10, 9
+            db "    Assessment Learning 1", 10, 9
+            db "    Pagtuturo ng Filipino sa Elementarya 1", 10, 9
+            db "    Teaching Math in the Primary Grades", 10, 9
+            db "    Teaching Music in the Elementary Grades", 10, 9
+            db "    Teaching English in the Elementary Grades - Language Arts", 10, 9
+            db "    Teaching Science in the Elementary Grades - Biology & Chemistry", 10, 9
+            db "    Adapted Physical Education and Recreation", 10, 9
+            db "    Behavior Management and Modification", 10, 9
+            db "    Dance and Sports 1", 10, 9, 0
 
-subjects4_1 db 10, 9, "Subjects for 4th Year: 1st Sem", 10, 9, "    Field Study 1", 10, 9, "    Teaching Internship", 10, 9, "    Career and Personality Development", 10, 9, 0
-subjects4_2 db 10, 9, "Subjects for 4th Year: 2nd Sem ", 10, 9, "    Field Study 1", 10, 9, "    Teaching Internship", 10, 9, "    Career and Personality Development", 10, 9, 0
+subjects2_2 db 10, 9, "Subjects for 2nd Year: 2nd Sem", 10, 9
+            db "    Readings in Philippine History", 10, 9
+            db "    The Teachers and the School Curriculum", 10, 9
+            db "    Assessment of Learning 2", 10, 9
+            db "    Teaching Social Studies in Primary Grades", 10, 9
+            db "    Teaching Literacy in the Elementary Grades", 10, 9
+            db "    Teaching Science in the Elementary Grades - Physics, Earth & Space", 10, 9
+            db "    Instructional Adaptations in Language & Literacy Instruction", 10, 9
+            db "    Instructional Adaptations in Mathematics & Science Instruction", 10, 9
+            db "    Dance and Sports 2", 10, 9, 0
 
+subjects2_summer db 10, 9, "Subjects for 1st Year: Summer", 10, 9
+            db "    Development of Individualized Education Plans", 10, 9
+            db "    Ethics (Disciplinal)", 10, 9
+            db "    Good Manners and Right Conduct", 10, 9
+    
+subjects3_1 db 10, 9, "Subjects for 3rd Year: 1st Sem", 10, 9
+            db "    Environmental Science", 10, 9
+            db "    Building and Enhancing New Literacies", 10, 9
+            db "    Research in Special Needs and Inclusive Education 1", 10, 9
+            db "    Teaching Social Studies in Intermediate Grades", 10, 9
+            db "    The Entrepreneurial Mind", 10, 9
+            db "    Edukasyong Pantahanan at Pangkabuhayan - Home Economics & Agri", 10, 9
+            db "    Teaching Arts in the Elementary Grades", 10, 9
+            db "    Instructional Adaptations for Teaching Content Areas", 10, 9, 0
 
-subjects1_sem db 10, 9, "1. 1st Sem:", 10, 9, "2. 2nd Sem", 10, 9, " Select which sem", 10, 9, 0
+subjects3_2 db 10, 9, "Subjects for 3rd Year: 2nd Sem", 10, 9
+            db "    The Teaching Profession", 10, 9
+            db "    The Teacher & the Community, Culture, and Org Leadership", 10, 9
+            db "    Research in Special Needs and Inclusive Education 2", 10, 9
+            db "    Teaching Math in the Intermediate Grades", 10, 9
+            db "    Edukasyong Pantahanan at Pangkabuhayan - Entrepreneurship", 10, 9
+            db "    Technology for Teaching and Learning in the Elementary Grades", 10, 9
+            db "    Teaching PE and Health in Elementary Grades", 10, 9
+            db "    Early Childhood Inclusive Education", 10, 9, 0
+
+subjects3_summer db 10, 9, "Subjects for 1st Year: Summer", 10, 9
+            db "    Pagtuturo ng Filipino sa Elementarya - Panitikan ng Pilipinas", 10, 9
+            db "    Malayuning Komunikasyon sa Wikang Filipino", 10, 9
+            db "    Reading Visual Arts", 10, 9
+    
+subjects4_1 db 10, 9, "Subjects for 4th Year: 1st Sem", 10, 9
+            db "    Field Study 1", 10, 9
+            db "    Field Study 2", 10, 9
+            db "    General Education", 10, 9
+            db "    Professional Education", 10, 9, 0
+
+subjects4_2 db 10, 9, "Subjects for 4th Year: 2nd Sem", 10, 9
+            db "    Teaching Internship", 10, 9
+            db "    Career and Personality Development", 10, 9, 0
+
+subjects1_sem db 10, 9, "1. 1st Sem", 10, 9, "2. 2nd Sem", 10, 9, "3. Summer", 10, 9, " Select which sem", 10, 9, 0
 choice1_sem db 4 dup (0)
 choice2_sem db 4 dup (0)
 choice3_sem db 4 dup (0)
@@ -84,27 +158,29 @@ show1:
     je show1_1
     cmp al, '2'
     je show1_2
-
+    cmp al, '3'
+    je show1_summer
     jmp askContinue   ; If invalid input, go back to asking to continue
     
 
 show2:
-     invoke StdOut, addr subjects1_sem
+    invoke StdOut, addr subjects1_sem
     invoke StdIn, addr choice2_sem, 4 
 
-    mov al, choice2_sem
+    mov al, [choice2_sem]
     cmp al, '1'
     je show2_1
     cmp al, '2'
     je show2_2
-
+    cmp al, '3'
+    je show2_summer
     jmp askContinue   ; If invalid input, go back to asking to continue
 
 
 
 
 show3:
-     invoke StdOut, addr subjects1_sem
+    invoke StdOut, addr subjects1_sem
     invoke StdIn, addr choice3_sem, 4 
 
     mov al, choice3_sem
@@ -112,7 +188,8 @@ show3:
     je show3_1
     cmp al, '2'
     je show3_2
-
+    cmp al, '3'
+    je show3_summer
     jmp askContinue   ; If invalid input, go back to asking to continueinvoke StdOut, addr subjects3
     
 
@@ -125,7 +202,6 @@ show4:
     je show4_1
     cmp al, '2'
     je show4_2
-
     jmp askContinue   ; If invalid input, go back to asking to continue
 
 
@@ -138,6 +214,10 @@ show1_2:
     invoke StdOut, addr subjects1_2
     jmp askContinue
 
+show1_summer:
+    invoke StdOut, addr subjects1_summer
+    jmp askContinue
+
 show2_1:
     invoke StdOut, addr subjects2_1
     jmp askContinue
@@ -146,12 +226,20 @@ show2_2:
     invoke StdOut, addr subjects2_2
     jmp askContinue
 
+show2_summer:
+    invoke StdOut, addr subjects2_summer
+    jmp askContinue
+
 show3_1:
     invoke StdOut, addr subjects3_1
     jmp askContinue
 
 show3_2:
     invoke StdOut, addr subjects3_2
+    jmp askContinue
+
+show3_summer:
+    invoke StdOut, addr subjects3_summer
     jmp askContinue
 
 show4_1:
